b3e3740b91aa69df92046ae71c9b370851d64521
samaclea <samaclea@cisco.com>
2024-02-24 00:13:53 -0500

    Changed updater to class and reduced api calls

--- src/boot.py
+++ src/boot.py
@@ -1,5 +1,5 @@
-import storage, board, digitalio, microcontroller, sys
-import updater
+import storage, board, digitalio, microcontroller, os
+import updater, secrets
 
 BTN1 = digitalio.DigitalInOut(board.BTN1)
 BTN1.direction = digitalio.Direction.INPUT
@@ -12,9 +12,14 @@ BTN2.pull = digitalio.Pull.UP
 # Hold BTN1 and BTN2 during reboot to update
 if not BTN1.value and not BTN2.value:
     storage.remount("/", readonly=False, disable_concurrent_write_protection=False)
-    with open("updater_out.txt", "w") as sys.stdout:
-        updater.run(debug=True)
-    microcontroller.reset()
+    #OTA = updater.Updater(secrets.WIFI_NETWORK, secrets.WIFI_PASS, secrets.GH_REPO, secrets.GH_SRC_FOLDER, debug=True)
+    #try:
+    #    special_files = OTA.run()
+    #    for tup in special_files:
+    #        os.rename(tup[0], tup[1])
+    #except Exception as e:
+    #    print("OTA failed: {}".format(e))
+    #microcontroller.reset()
 
 # Uncomment to for updater testing
 #storage.remount("/", readonly=False, disable_concurrent_write_protection=False)
