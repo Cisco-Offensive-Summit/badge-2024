39a555d4b4ac3f3f25c723fc74cdca9f19c7a285
Phillip <phillipmcknigh@gmail.com>
2024-04-03 19:56:59 -0400

    added registration app

--- src/secrets.py
+++ src/secrets.py
@@ -9,3 +9,6 @@ GH_TOKEN     = ""
 GH_REPO      = "https://github.com/Cisco-Offensive-Summit/badge-2024"
 GH_BRANCH    = "main"
 GH_SRC_FOLDER= "src"
+
+## Registration
+HOST_ADDRESS = 'https://badger.becomingahacker.com/'
