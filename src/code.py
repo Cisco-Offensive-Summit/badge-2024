3bfa4bb39363193d1838654dc8128f1c6e728ce9
samaclea <samaclea@cisco.com>
2024-04-09 13:33:25 -0400

    Badge info URL

--- src/code.py
+++ src/code.py
@@ -5,6 +5,8 @@ except ImportError:
     from displayio import FourWire
 from adafruit_st7735r import ST7735R
 from pdepd import EPD
+from adafruit_display_text import label, wrap_text_to_pixels
+import time, terminalio
 
 def main():
     import adafruit_imageload, io
@@ -22,6 +24,16 @@ def main():
     # Load and display logo
     epd.image('/img/epd_logo.bmp')
     epd.draw()
+
+    group = displayio.Group()
+    text_area = label.Label(terminalio.FONT, text='\n'.join(wrap_text_to_pixels("Please visit https://badger.becomingahacker.com/badge_info to update your badge", 126, terminalio.FONT)))
+    text_area.anchor_point = (0,0)
+    text_area.anchored_position = (2, 2)
+    group.append(text_area)
+
+    lcd.show(group)
+    while True:
+        time.sleep(1)
 
 
 if __name__ == "__main__":
