0f35788158535e8878af4ef255b0c1383f856cbb
Phillip <phillipmcknigh@gmail.com>
2025-05-12 11:01:07 -0400

    added appStore icon. and changed wifi creds in secrets.py

--- src/secrets.py
+++ src/secrets.py
@@ -1,8 +1,8 @@
 # Secrets!
 
 ## Wifi Network
-WIFI_NETWORK = "Badge-Wifi"
-WIFI_PASS    = "Cxlabs!13579"
+WIFI_NETWORK = "os2025-badge"
+WIFI_PASS    = "mV*M66s8TkY!rYcnDpVY"
 
 ## Registration
 HOST_ADDRESS = 'https://badger.becomingahacker.com/'
